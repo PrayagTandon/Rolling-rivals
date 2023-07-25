3173a7d9cb48eabf2ac4ab426ec54ac1058ae1ca
Prayag Tandon <tandonprayag99@gmail.com>
2023-07-25 10:22:52 +0530

    writing JS

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// SELECTING THE ELEMENTS
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('.score--0');
+const score1El = document.querySelector('score--1');
+const current0El = document.querySelector('.current--0');
+const current1El = document.querySelector('.current--1');
+
+// SELECTING THE BTN and IMG
+const diceEl = document.querySelector('.dice-img');
+const btnNewEl = document.querySelector('.btn--reset');
+const btnRollEl = document.querySelector('.btn--roll');
+const btnHoldEl = document.querySelector('.btn--hold');
